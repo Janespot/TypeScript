57bf833d91e8381cb615533da58fe3e5049529eb
Junell777 <janejunell777@gmail.com>
2022-10-30 12:07:22 +0300

    arrays

--- arrays.ts
+++ arrays.ts
@@ -3,3 +3,13 @@ names.push("Jane");// no error
 names.push(3); // error: type"number"
 console.log(names);
 
+//to avoid arrays from being changed use "readonly" keyword"
+
+
+const names: readonly string[] = ["Jane"];
+names.push("June");//error at the string is readonly
+console.log(names);
+
+
+
+
