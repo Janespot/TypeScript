eadf0c5a8a022308fb29945daa5b56ea32a43c01
Junell777 <janejunell777@gmail.com>
2022-10-30 11:22:12 +0300

    special types

--- specialTypes.ts
+++ specialTypes.ts
@@ -3,3 +3,20 @@
 let u: any = true;
 u = "string";
 console.log(Math.round(u));
+
+//"any"should be avoided at all cost
+//
+//special type "unknown"
+
+let w: unknown = 1;
+w = "string";
+
+w = {
+	runANonExistentMethod: () => {
+		console.log("I think therefore I am");
+	}
+} as { runANonExistentMethod: () => void }
+
+if(typeof w === 'object' && w !== null) {
+	(w as { runANonExistentMethod: Function }).runANonExistentMethod();
+}
