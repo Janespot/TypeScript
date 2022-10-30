8f5b46f8197223aed5771b0024dd8011d25e9eb0
Junell777 <janejunell777@gmail.com>
2022-10-30 12:11:50 +0300

    arrays

--- arrays.ts
+++ arrays.ts
@@ -10,6 +10,13 @@ const names: readonly string[] = ["Jane"];
 names.push("June");//error at the string is readonly
 console.log(names);
 
+//type inference
+
+const numbers = [1, 2, 3];// inferred to type number
+numbers.push(4);//no error
+numbers.push("2");//error 
+let head: number = numbers[0];
+
 
 
 
