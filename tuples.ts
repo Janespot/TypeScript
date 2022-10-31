ac63d697b0ca19b1276dbde498a44eb165806abe
Junell777 <janejunell777@gmail.com>
2022-10-31 15:20:31 +0300

    tuples

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+// This is a typed array that has pre-defined length and types
+// for each index
+
+
+let myTuple: [number, boolean, string];
+myTuple = [7, true, 'Janespot'];
+
+console.log(myTuple);
