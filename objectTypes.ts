0417d34f6c87662305f332abe01c096f2833cd1f
Junell777 <janejunell777@gmail.com>
2022-10-31 16:04:02 +0300

    object types

--- objectTypes.ts
+++ objectTypes.ts
@@ -6,3 +6,10 @@ const student: {name: string, year: number, age: number } = {
 
 console.log(student);
 
+//TypeScript can infer types of properties based on their values
+//For optional properties i.e. properties that are not assigned at the moment:
+
+const car: { type: string, mileage?: number } = {
+	type: "Toyota"
+};
+car.mileage = 2000;
