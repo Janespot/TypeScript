91690348d421eae9876c766433f082715cffb2a4
Junell777 <janejunell777@gmail.com>
2022-10-31 15:48:08 +0300

    tuples

--- tuples.ts
+++ tuples.ts
@@ -12,3 +12,12 @@ console.log(myTuple);
 
 const myReadonlyTuple: readonly [number, boolean, string] = [6, false, 'Hello'];
 myReadonlyTuple.push('Janespot');//gives an error as this tuple is readonly
+
+//named tuples
+const graph: [x: number, y: number] = [55.2, 41.3];
+
+
+
+//since tuples are arrays, we can also destructure them:
+const graph: [number, number] = [55.2, 41.3];
+const [x, y] = graph;
