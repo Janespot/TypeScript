a014801e47fe5957a945c1af64575cb31c242ff5
Junell777 <janejunell777@gmail.com>
2022-11-03 09:00:12 +0300

    union

--- union.ts
+++ union.ts
@@ -1,6 +1,17 @@
+// union types is used when a value can be more than a single type using the | (OR) operator
 function printStatusCode(code: string | number){
 	console.log(`My status code is ${code}.`)
 }
 
 printStatusCode(404);
 printStatusCode('404');
+
+
+
+//union error - when working with union, know your types to avoid type error e.g
+
+function printStatusCode(code: string | number) {
+  console.log(`My status code is ${code.toUpperCase()}.`) // error: Property 'toUpperCase' does not exist on type 'string | number'. Property 'toUpperCase' does not exist on type 'number'
+}
+
+
